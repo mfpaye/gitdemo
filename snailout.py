da2a861fa2d506652970b59225892cfa4165082c
Marietou <mfpaye@gmail.com>
2023-02-05 18:17:25 -0500

    update snailout

--- snailout.py
+++ snailout.py
@@ -104,15 +104,15 @@ print(x)
 1
 ###############
 ### easier way to do this:
-import numpy as np
+#import numpy as np
 
-def snail(array):
-    m = []
-    array = np.array(array)
-    while len(array) > 0:
-        m += array[0].tolist()
-        array = np.rot90(array[1:])
-    return m
+# def snail(array):
+#     m = []
+#     array = np.array(array)
+#     while len(array) > 0:
+#         m += array[0].tolist()
+#         array = np.rot90(array[1:])
+#     return m
 
 
 ###############
